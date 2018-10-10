1d636b8aac4e6169670067780b53089700c229f7
brianyu18 <brianyu1990@gmail.com>
2018-10-10 20:07:26 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
 require 'pry'
 
 class String
+  # attr_accessor :sentence?, :question?, :exclamation?, :count_sentences
+
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.find_
+    index[-1] == "?"
   end
 
   def exclamation?
-
+    self.index[-1] == "?"
   end
 
   def count_sentences
-
+    self.count_sentences
   end
 end
+
+# NewString = String.new
+
+# NewString.sentence?
+
+# NewString.question?
+
+# NewString.exclamation?
+
+# NewString.count_sentences
